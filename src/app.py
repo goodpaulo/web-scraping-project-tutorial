3f807962f183bbd4397f67a65093beb392d3c166
goodpaulo <36121052+goodpaulo@users.noreply.github.com>
2024-09-03 02:16:16 +0000

    not done again

--- src/app.py
+++ src/app.py
@@ -22,11 +22,39 @@ soup = BeautifulSoup(response,"html.parser")
 tables = soup.find_all("table")
 
 #tesla_tables = 
-print(tables)
+tables_with_date_header = []
+
+for table in tables:
+    # Find all th elements in the thead of the current table
+    if table.find('thead') is not None:
+        headers = table.find('thead').find_all('th')
+    print(headers)
+    
+    # Check if any header contains the text "Date"
+    if any(header.get_text(strip=True) == "Date" for header in headers):
+        tables_with_date_header.append(table)
+
+for table in tables:
+    if (header.get_text(strip = True) == "Date" for header in headers):
+        print(table)
+
+# Print or process the tables with the header "Date"
+#for table in tables_with_date_header:
+    #print(table.prettify())
 
 price = soup.find("td", class_="text-right")
 date = soup.find("td")
+tables_with_th = []
 
+# Iterate through each table
+for table in tables:
+    # Check if there is any <th> element in the table
+    if table.find_all('th'):
+        tables_with_th.append(table)
+
+# Print or process the tables with <th> elements
+for table in tables_with_th:
+    print(table.prettify())
 #gfg = soup.find_all(lambda tag: tag.name == "strong" and text in tag.text)
 
 tesla_revenue = pd.DataFrame(columns = ["Date", "Revenue"])
